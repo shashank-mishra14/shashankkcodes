ded4488ddfd07068ca82837138a1f303181b1766
Shashank Mishra <shashank.mishra.dev14@gmail.com>
2025-03-13 15:15:39 +0530

    feat: added the details

---
+++ src/utils/consts/index.ts
@@ -0,0 +1,21 @@
+export const ORIGIN = 'shashankcodes.me' as const
+export const PROTOCOL = 'https' as const
+export const HOST = `${PROTOCOL}://${ORIGIN}` as const
+
+export const SITE_TITLE = 'Shashank'
+export const DEFAULT_DESCRIPTION =
+  'Web & Frontend developer, passionate about building apps from scratch, perfecting both backend and frontend.'
+export const DEFAULT_IMAGE_URL = '/img/baner.png'
+
+export const EMAIL = 'zlvsky@icloud.com'
+export const NAME = 'Krzysztof Zaleski'
+export const POSITION = 'full stack developer'
+export const AUTHOR_DESC =
+  'Web & Frontend developer based in India,  Passionate about creative solutions and building apps from scratch.'
+
+export const GITHUB_URL = 'https://github.com/shashank-mishra14'
+export const TWITTER_URL = 'https://twitter.com/Sshashankk01'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/shashank-mishra14'
+export const RESUME_URL =
+  'https://drive.google.com/file/d/1Qy1i3blClZLcILpFZ4fbBPln1g8ZUqQU/view?usp=sharing'
+export const GUMROAD_URL = 'https://shashankmishra.gumroad.com/'
